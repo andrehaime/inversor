9738cce711dfdf8651c53e8f6b865c84fac30b19
Andre Ubuntu VB <alhc02@gmail.com>
2020-05-14 01:39:28 -0300

    adicionados comentários referentes ao programa

--- src/main.c
+++ src/main.c
@@ -1,10 +1,10 @@
-/* Data de submissao:
- * Nome:
- * RA:
+/* Data de submissao: 14/05/2020
+ * Nome: André Luis Haime Coutinho
+ * RA: 166329
  */
 
 #include <stdio.h>
-#include <string.h>
+
 
 const int tam_buffer=100;
 char pilha[100];
@@ -35,24 +35,38 @@ int main() {
   int era_letra =0;
   int tamanho_palavra =0;
   
+ 
   fgets(buffer, tam_buffer, stdin);
-  //printf("%s", buffer);
   
+  // limpa o vetor pilha, por seguranca 
   for (int i=0; i< 100; i++) pilha[i] = '\n';
-  //printf("%d", n_pilha);
+  
+  
   for (int i =0; i<100; i++){
+    // Percorre o buffer
     if (buffer[i] != ' ' && buffer[i] != '\n'){
+      // se encontrar um caracter que não seja letra ou 
+      // quebra de linha, adiciona no buffer
       push( buffer[i]);
+      
+      // variavel para indicar que o ultimo caracter encontrado
+      // era letra
       era_letra = 1;
     }
     else if ((buffer[i] == ' ' || buffer[i] == '\n') && era_letra){
-     
+      // se encontrar um espaco ou quebra de linha e o ultimo caracter lido
+      // tiver sido uma letra, tira todos os itens do buffer;
       tamanho_palavra = n_pilha;
       for (int j=0; j<tamanho_palavra; j++) printf("%c", pop());
       era_letra =0;
     }
+    // se encontrar um espaco e o ultimo caracter não for uma letra,
+    // imprime um espaco
     if (buffer[i] == ' ' && ! era_letra) printf(" ");
+    // se encontrar uma quebra de linha e o ultimo caracter não for uma letra,
+    // imprime uma quebra de linha
     else if (buffer[i] == '\n' && ! era_letra) printf("\n");
+    // para o for quando encontrar uma quebra de linha
     if (buffer[i] == '\n') break;
   }
 
